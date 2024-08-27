6fe904963da67bac4c9f0c80617acbe25e837952
Anar <anar.sol.dev@gmail.com>
2024-08-27 22:10:21 +0100

    update

--- .eleventy.js
+++ .eleventy.js
@@ -50,7 +50,7 @@ function getHtmlFromNode(node) {
                 const url = new URL(childNode.value.url);
                 return `
                     <figure>
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed${url.pathname}${url.search}"
+                        <iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed${url.pathname}${url.search}"
                             title="YouTube video player" frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
